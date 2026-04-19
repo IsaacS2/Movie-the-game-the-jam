49918c1cb984027f3490adae7ce8f98359bc5a36
IsaacS2Hud <sanchezisaac809@gmail.com>
2026-04-19 01:06:02 -0500

    Adds basic movement

---
+++ scripts/PlayerCamera.gd
@@ -0,0 +1,73 @@
+class_name PlayerCamera
+
+extends Camera3D
+
+enum FACING {EAST, NORTH, WEST, SOUTH}
+
+@export var facility : GridMap
+var cameraHeight : Vector3
+var movingDirection : Vector3
+var movingInput : int
+var turningDirection : int
+var currentRotation = FACING.EAST
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	cameraHeight = Vector3.UP * .5
+	_turn_to_direction(currentRotation)
+	if (facility):
+		position = facility.map_to_local(Vector3i(0,0,0)) + cameraHeight
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	movingInput = int(Input.is_action_just_pressed("ui_down")) - int(Input.is_action_just_pressed("ui_up"))
+	turningDirection = int(Input.is_action_just_pressed("ui_left")) - int(Input.is_action_just_pressed("ui_right"))
+	
+	if (movingInput != 0):
+		var desiredSpace : Vector3 = facility.local_to_map(position)
+		#print(desiredSpace)
+		#print(movingInput)
+		desiredSpace += movingInput * movingDirection
+		#print(desiredSpace)
+		
+		var cellCheck : int = facility.get_cell_item(desiredSpace)
+		desiredSpace = facility.map_to_local(desiredSpace)
+		#print(desiredSpace)
+		
+		if (facility and cellCheck > -1):
+			position = desiredSpace + cameraHeight
+			#print(position)
+	
+	elif (turningDirection != 0):
+		currentRotation += turningDirection
+		# full clockwise rotation, return to facing east
+		if (currentRotation > FACING.SOUTH):
+			currentRotation = FACING.EAST
+		# counter-clockwise rotation, face south
+		if (currentRotation < FACING.EAST):
+			currentRotation = FACING.SOUTH
+		
+		_turn_to_direction(currentRotation)
+	
+	movingInput = 0
+	turningDirection = 0
+
+func _turn_to_direction(newDirection : FACING) -> void:
+	currentRotation = newDirection
+	
+	match currentRotation:
+		FACING.EAST:
+			movingDirection = Vector3(0, 0, 1)
+		FACING.NORTH:
+			movingDirection = Vector3(1, 0, 0)
+		FACING.WEST:
+			movingDirection = Vector3(0, 0, -1)
+		FACING.SOUTH:
+			movingDirection = Vector3(-1, 0, 0)
+	
+	print(currentRotation)
+	
+	transform.basis = Basis()
+	transform.basis = Basis(Vector3.UP, (PI * currentRotation) / 2) * transform.basis
+	 
